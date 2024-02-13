268812e07b9ac7bd11ffe63bbe634966f4bc2453
hader-hussein <91197408+hader-hussein@users.noreply.github.com>
2024-02-13 15:53:25 +0200

    13-2

---
+++ assets/vendor/js/accordion.js
@@ -0,0 +1,24 @@
+$('.accordion-button').on('keydown',function (e) {
+ 
+ 
+     $('.accordion-collapse').collapse("toggle")
+    
+     
+   })
+   $('.tabel-table').on('keydown','tr td',function (e) {
+    var keyCode = e.keyCode;
+    if (keyCode !== 9) return;
+
+    var $this = $(this),
+        $lastTr = $('tr:last', $('table')),
+        $lastTd = $('td:last', $lastTr);
+
+    
+    if (($(e.target).closest('td')).is($lastTd)) {
+     
+
+        $lastTr.after($lastTr.clone());
+      
+    }
+    
+});
